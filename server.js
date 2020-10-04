93ca12bc102a25cd13927e048fd24d7d176eaef7
Gabriel <creeplays@gmail.com>
2020-10-04 15:13:56 -0300

    HttpServer para deploy no heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+
+app.use(express.static('./dist/AngularTetrisPage'));
+
+app.get('/*',(req,res)=>
+{
+    res.sendFile('index.html',{root:'dist/AngularTetrisPage'})
+});
+
+app.listen(process.env.PORT || 8080);
